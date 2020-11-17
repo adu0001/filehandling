7ebd1cc905079dfad3c840f3c84920659a28d388
adarsh <adarshmohan42@gmail.com>
2020-11-17 23:56:20 +0530

    Initial commit

---
+++ src/main/java/com/home/filehandling/FilehandlingApplication.java
@@ -0,0 +1,13 @@
+package com.home.filehandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilehandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilehandlingApplication.class, args);
+	}
+
+}
