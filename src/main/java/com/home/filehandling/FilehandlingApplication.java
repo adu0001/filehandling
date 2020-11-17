be3a8744e4961d32ef94a9e7194e3ed46943c73d
adarsh <adarshmohan42@gmail.com>
2020-11-18 00:08:01 +0530

    project update

--- src/main/java/com/home/filehandling/FilehandlingApplication.java
+++ src/main/java/com/home/filehandling/FilehandlingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FilehandlingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FilehandlingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FilehandlingApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/home/filehandling/api/CsvApi.java
@@ -0,0 +1,4 @@
+package com.home.filehandling.api;
+
+public interface CsvApi {
+}
